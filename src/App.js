bbf045adceed618b7c1e94f69b7a7bb2eaa806e5
*ladeed <muhammed.ladeed123@gmail.com>
2021-09-01 20:35:27 +0530

    animation completed

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import logo from './image/logo.png'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <div className="child">
+
+    </div>
+    
+    </div>
+  );
+}
+
+export default App;
